13018dd3256606847c005d8c279af733a64b2240
Irfan Ahmed <irfandhk@gmail.com>
2021-07-19 17:57:03 +0200

    add: gifting concept for an employee

--- app/finders/gift_suggestion_finder.rb
+++ app/finders/gift_suggestion_finder.rb
@@ -1,46 +1,35 @@
-class GiftFinderService
-  attr_reader :employee_id, :employee
+class GiftSuggestionFinder
+  attr_reader :employee
   
-  def initialize(employee_id)
-    @employee_id = employee_id
+  def initialize(employee)
+    @employee ||= employee
   end
   
   def suggest
     # if employee already have a gift lets not suggest anything!
-    return [] if employee_gifts.present?
-
-    employee_interests
+    return [] if employee.gifts.present?
+    
+    suggest_gifts
   end
   
   private
-    
-    def employee_cache_key
-      "employee-#{employee_id}"
-    end
-    
-    def employee_category_cache_key
-      "employee-category-cache-key-#{employee_id}"
-    end
-    
-    def employee
-      @employee ||= Rails.cache.fetch(employee_cache_key) do
-        Employee.preload(:gifts).find(employee_id)
-      end
-    end
-    
-    # Avoid n+1 query
-    # Caching for to avoid redundancy
-    def employee_gifts
-      @employee_interests ||= Rails.cache.fetch(employee_cache_key) do
-        @employee.preload(:interests).pluck(:name)
-      end
-    end
-
     # Avoid n+1 query
     # Caching for to avoid redundancy
     def employee_interests
-      @employee_interests ||= Rails.cache.fetch(employee_cache_key) do
-        @employee.preload(:interests).pluck(:name)
+      @employee_interests ||= employee.interests.pluck(:name)
+    end
+    
+    # Avoid n+1 query
+    # Caching for to avoid redundancy
+    def suggest_gifts
+      Rails.cache.fetch(gift_suggestion_cache_key) do
+        @suggestion ||= Gift.preload(:categories)
+                            .where(categories: { name: employee_interests })
+                            .pluck(:name)
       end
+    end
+
+    def gift_suggestion_cache_key
+      "gift/suggestion/#{employee.cache_key}"
     end
 end
--- app/services/gift_service.rb
+++ app/services/gift_service.rb
@@ -1,3 +1,25 @@
 class GiftService
+  attr_reader :employee_id
 
+  def initialize(employee_id)
+    @employee_id = employee_id
+  end
+
+  def assign
+    return false if employee.gifts.present?
+
+    ActiveRecord::Base.transaction do
+      employee.gifts << suggested_gifts.first
+    end
+  end
+
+  private
+
+  def suggested_gifts
+    @suggested_gifts ||= GiftSuggestionFinder.new(employee).suggest
+  end
+
+  def employee
+    @employee ||= Employee.find(employee_id)
+  end
 end
